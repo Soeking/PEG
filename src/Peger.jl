963faf17e1074b97cd71c2e0f83c896bbc4a872a
Soeking <great.from.g@gmail.com>
2020-08-23 23:39:50 +0900

    abst Rule

--- src/Peger.jl
+++ src/Peger.jl
@@ -5,7 +5,16 @@ include("rule.jl")
 function readPEG(filename::String)
     texts = open(filename,"r") |> readlines
     filter!(!isempty, texts)
-    PEG()
+    p = PEG()
+    for text = texts
+        rule = Rule(text)
+        if rule == nothing
+            println("Error")
+        else
+            push!(p.rules,rule)
+        end
+    end
+    p
 end
 
 end
--- src/rule.jl
+++ src/rule.jl
@@ -5,6 +5,18 @@ end
 mutable struct Rule
     name::String
     expressions::Array{Expression}
+
+    function Rule(line::String)
+        check = match(r"\w+\s*<-.*$", line)
+        len = check.match
+        if length(line) == length(check)
+            m = match(r"^(\w+)\s*<-\s*(.*)$", line)
+            (name, expr) = m.captures
+            new(name, Expression(expr))
+        else
+            nothing
+        end
+    end
 end
 
 struct PEG
