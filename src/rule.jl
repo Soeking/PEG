f56347fec17633225486884aec4169b590127d45
Soeking <great.from.g@gmail.com>
2020-08-27 18:33:51 +0900

    init rule type

--- src/rule.jl
+++ src/rule.jl
@@ -1,16 +1,41 @@
+@enum Pattern begin
+    rule_name
+    choice          #/
+    symbol          #""
+    group           #()
+    one_of          #[]
+    plus            #+
+    asterisk        #*
+    question        #?
+    everything      #.
+    minus           #-
+    escape          #\
+    not             #^
+end
+
+mutable struct PartsType
+    pat::Pattern
+    child1::Array{PartsType}
+    child2::Array{PartsType}
+end
+
 mutable struct Expression
     parts::Array{Any}
+
+    function Expression(part::String)
+
+    end
 end
 
 mutable struct Rule
     name::String
-    expressions::Array{Expression}
+    expressions::Expression
 
     function Rule(line::String)
-        check = match(r"\w+\s*<-.*$", line)
+        check = match(r"^\s*\w+\s*<-.*$", line)
         len = check.match
-        if length(line) == length(check)
-            m = match(r"^(\w+)\s*<-\s*(.*)$", line)
+        if length(line) == length(len)
+            m = match(r"(\w+)\s*<-\s*(.*)", line)
             (name, expr) = m.captures
             new(name, Expression(expr))
         else
